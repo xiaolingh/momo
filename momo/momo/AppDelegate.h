1c1ba2fd7cc64b3a428468706cc6b4d6c06a17af
Pigtail <Pigtail@a.local>
2015-12-16 13:24:01 +0800

    初始化

---
+++ momo/momo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  momo
+//
+//  Created by Pigtail on 15/12/16.
+//  Copyright © 2015年 xmg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ momo/momo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  momo
+//
+//  Created by Pigtail on 15/12/16.
+//  Copyright © 2015年 xmg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
